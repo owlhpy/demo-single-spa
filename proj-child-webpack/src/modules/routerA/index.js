efcb0b86f98846cf9fee43347cda4725789c7f4f
owlhpy <1016297688@qq.com>
2022-06-28 15:43:16 +0800

    remove commet

--- proj-child-webpack/src/modules/routerA/index.js
+++ proj-child-webpack/src/modules/routerA/index.js
@@ -1,20 +1,4 @@
-/*
- * @Author: peiyanh
- * @Date: 2022-06-15 16:30:18
- * @LastEditTime: 2022-06-15 16:34:41
- * @LastEditors: peiyanh
- * @Description: 
- * @FilePath: /ar-react-cli/template/antd-react-router/src/modules/routerA/index.js
- * Copyright (c) 2004-2021 i-Sprint Technologies, Inc.
- *  address: 
- *  All rights reserved. 
- *  
- *  This software is the confidential and proprietary information of 
- *  i-Sprint Technologies, Inc. ('Confidential Information').  You shall not 
- *  disclose such Confidential Information and shall use it only in 
- *  accordance with the terms of the license agreement you entered into 
- *  with i-Sprint. 
- */
+
 import React from "react";
 
 const RouterA = () => {
--- proj-child1/src/modules/routerA/index.js
+++ proj-child1/src/modules/routerA/index.js
@@ -1,20 +1,4 @@
-/*
- * @Author: peiyanh
- * @Date: 2022-06-27 16:32:56
- * @LastEditTime: 2022-06-27 16:32:56
- * @LastEditors: peiyanh
- * @Description: 
- * @FilePath: /single-spa/proj-child1/src/modules/routerA/index.js
- * Copyright (c) 2004-2021 i-Sprint Technologies, Inc.
- *  address: 
- *  All rights reserved. 
- *  
- *  This software is the confidential and proprietary information of 
- *  i-Sprint Technologies, Inc. ('Confidential Information').  You shall not 
- *  disclose such Confidential Information and shall use it only in 
- *  accordance with the terms of the license agreement you entered into 
- *  with i-Sprint. 
- */
+
 import React from "react";
 const RouterA = () => {
 	return (

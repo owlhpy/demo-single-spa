8617d97f7760eab87f3373f8fae1c72a32b70e0b
owlhpy <1016297688@qq.com>
2022-06-28 15:38:40 +0800

    Update index.js

--- proj-child-webpack/src/modules/app/index.js
+++ proj-child-webpack/src/modules/app/index.js
@@ -1,20 +1,4 @@
-/*
- * @Author: peiyanh
- * @Date: 2022-06-09 13:59:27
- * @LastEditTime: 2022-06-28 14:52:28
- * @LastEditors: peiyanh
- * @Description: 
- * @FilePath: /single-spa/proj-child-webpack/src/modules/app/index.js
- * Copyright (c) 2004-2021 i-Sprint Technologies, Inc.
- *  address: 
- *  All rights reserved. 
- *  
- *  This software is the confidential and proprietary information of 
- *  i-Sprint Technologies, Inc. ('Confidential Information').  You shall not 
- *  disclose such Confidential Information and shall use it only in 
- *  accordance with the terms of the license agreement you entered into 
- *  with i-Sprint. 
- */
+
 import React from "react";
 import './index.less'
 import styles from './index.module.less'

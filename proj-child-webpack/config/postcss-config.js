712d0f80bb6321d080fe506e6f010bf7225d87e4
owlhpy <1016297688@qq.com>
2022-06-28 15:38:00 +0800

    Update postcss-config.js

--- proj-child-webpack/config/postcss-config.js
+++ proj-child-webpack/config/postcss-config.js
@@ -1,20 +1,4 @@
-/*
- * @Author: peiyanh
- * @Date: 2022-06-15 15:44:47
- * @LastEditTime: 2022-06-15 15:44:47
- * @LastEditors: peiyanh
- * @Description: 
- * @FilePath: /ar-react-cli/template/antd-react-middle/config/postcss-config.js
- * Copyright (c) 2004-2021 i-Sprint Technologies, Inc.
- *  address: 
- *  All rights reserved. 
- *  
- *  This software is the confidential and proprietary information of 
- *  i-Sprint Technologies, Inc. ('Confidential Information').  You shall not 
- *  disclose such Confidential Information and shall use it only in 
- *  accordance with the terms of the license agreement you entered into 
- *  with i-Sprint. 
- */
+
 module.exports = {
 	plugins: (loader) => [
 		require('postcss-import')({ root: loader.resourcePath }),
